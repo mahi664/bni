b526adfa44c4ebea480f30313e2e00d9bb38cbf5
mahi664 <mahighuge664@gmail.com>
2020-09-07 15:07:25 +0530

    Pagination

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -56,7 +56,18 @@ export class InventoryComponent implements OnInit {
    categories = ['Test Category 1','Test Category 2','Test Category 3','Test Category 4','Test Category 5'];
   constructor() { }
 
+  noOfPages=0;
+  i;
+  pageOfProducts=[];
+  pages=[];
+  currPage=1;
   ngOnInit() {
+    this.noOfPages = Math.ceil(this.products.length/5);
+    for(this.i=1;this.i<=this.noOfPages;this.i++){
+      this.pages.push(this.i);
+    }
+    console.log(this.products);
+    this.pageOfProducts = this.products.slice(0,5);
   }
 
   expandProduct(entity_id){
@@ -81,5 +92,14 @@ export class InventoryComponent implements OnInit {
       document.getElementById(product_up_button_id).style.display = "none";
       document.getElementById(product_down_button_id).style.display = "block";
       document.getElementById(content_id).style.display = "none";
+  }
+
+  changePageContent(pageNo){
+    if(pageNo<=0)
+      pageNo=1;
+    else if(pageNo>=this.noOfPages)
+      pageNo=this.noOfPages;
+    this.pageOfProducts = this.products.slice((pageNo-1)*5,(pageNo-1)*5+5);
+    this.currPage=pageNo;
   }
 }
